49f93f95e2cb300c3e325e0a4eec8dcddf9126d9
sud <brunohallef@gmail.com>
2022-02-20 11:39:48 -0300

    Enviando projeto para github

---
+++ src/test/java/br/ce/wcaquino/TasksFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ce.wcaquino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
